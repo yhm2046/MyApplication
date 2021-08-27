c28536759e9f005de076f1eb714a0c0e084037c2
evan <mm11751@outlook.com>
2021-08-27 19:50:20 +0800

    add some gps algorihm

--- app/src/main/java/com/example/myapplication/AdanGPS.java
+++ app/src/main/java/com/example/myapplication/AdanGPS.java
@@ -2,11 +2,62 @@ package com.example.myapplication;
 
 import android.util.Log;
 
+import java.util.Arrays;
+
 /**
  * build 2021.8.26  Thursday
  * gps相关方法
  */
 public class AdanGPS {
+    private static final String TAG="AdanGPS:xwg";
+    public static void decodeMsg(String msg) {
+        if (!msg.startsWith("4D442E2E")) {
+            Log.i(TAG,"not start with 4D442E2E");
+            return;
+        }
+        if (msg == null || msg.length() <= 12) {
+            //长度过短
+            Log.i("decodeMsg", "Too Short");
+            return;
+        }
+        byte[] totalBytes = hexStringToByteArray(msg);   //[77, 68, 46, 46, 2, -73,...]转成十进制数字
+        Log.i(TAG,"totalBytes:"+ Arrays.toString(totalBytes));
+        Log.i(TAG,"totalBytes size:"+ totalBytes.length);
+       dHex("decodeMsg", totalBytes, totalBytes.length);   //4D 44 2E 2E 02 B7。。，打印十六进制数字
+        if (totalBytes[totalBytes.length - 1] != getCheckSum(totalBytes)) {
+            //检验出错
+            Log.i("decodeMsg", "CheckSum Error");
+            return;
+        }
+        if (totalBytes[0] == 0x4D
+                && totalBytes[1] == 0x44
+                && totalBytes[2] == 0x2E
+                && totalBytes[3] == 0x2E) { //message header头文件
+            int messageSize = ((totalBytes[4] & 0xff) << 8) | (totalBytes[5] & 0xff);
+            Log.i("decodeMsg", "messageSize : " + messageSize);
+            if (messageSize == totalBytes.length - 9) {//message header 4位+message size 2位
+//                                                        +dispatcher message length 2位+ checksum 1 位
+                int applicationID = ((totalBytes[10] & 0xff) << 8) | (totalBytes[11] & 0xff);
+                Log.i("decodeMsg", "applicationID : " + Integer.toHexString(applicationID));
+                switch (applicationID) {
+                    case 0x3001:
+//                        decodeGpsMsg(msg);
+                        Log.i(TAG,"定位信息查询设置");
+                        break;
+                    case 0x3003:
+//                        decodeNetMsg(msg);
+                        Log.i(TAG,"网络信息查询设置");
+                        break;
+                    case 0x3005:
+//                        decodeVin(msg);
+                        Log.i(TAG,"VIN信息查询设置");
+                        break;
+                    default:Log.i(TAG,"nothing!");
+                }
+            }
+        }
+    }
+
     /**
      * 16进制表示的字符串转换为字节数组，//4d442e2e ->  [77, 68, 46, 46]
      *
@@ -18,7 +69,8 @@ public class AdanGPS {
         byte[] b = new byte[len / 2];
         for (int i = 0; i < len; i += 2) {
             // 两位一组，表示一个字节,把这样表示的16进制字符串，还原成一个字节
-            b[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4) + Character.digit(s.charAt(i + 1), 16));
+            b[i / 2] = (byte) ((Character.digit(s.charAt(i), 16) << 4)
+                    + Character.digit(s.charAt(i + 1), 16));
         }
         return b;
     }   //hexStringToByteArray
@@ -31,13 +83,14 @@ public class AdanGPS {
     public static byte getCheckSum(byte[] bytes) {
         byte b = 0x00;
         for (int i = 0; i < bytes.length-1 ; i++) {
-            b = (byte) (b ^ bytes[i]);
+//            b = (byte) (b ^ bytes[i]);
+            b^=bytes[i];
         }
         return b;
     }
 
     /**
-     * 字节还原成16进制数，[77, 68, 46, 46] -> 4D 44 2E 2E
+     * 10进制字节还原成16进制数，[77, 68, 46, 46] -> 4D 44 2E 2E
      * @param tag   String:打印的标志
      * @param buf   byte[]:字节数组
      * @param len   int：字节数组长度
--- app/src/main/java/com/example/myapplication/Math.java
+++ app/src/main/java/com/example/myapplication/Math.java
@@ -5,6 +5,55 @@ package com.example.myapplication;
  * 数学算法
  */
 public class Math {
+    /**
+     * 将字节数组转换为16进制字符串
+     * @param bytes
+     * @return
+     */
+    public static String binaryToHexString(byte[] bytes) {
+        if (bytes == null || bytes.length == 0) return "";
+        String hexStr = "0123456789ABCDEF";
+        StringBuilder result = new StringBuilder("");
+        String hex = "";
+        for (byte b : bytes) {
+            hex = String.valueOf(hexStr.charAt((b & 0xF0) >> 4));
+            hex += String.valueOf(hexStr.charAt(b & 0x0F));
+            result.append(hex);
+            //result += hex + " ";//可加空格分割
+        }
+        return result.toString();
+    }
+
+    /**
+     * 将一组十六进制字符转换成字节数组
+     * @param hexString
+     * @return
+     */
+    public static byte[] hexStringToBytes(String hexString) {
+        if (hexString == null || hexString.equals("")) {
+            return null;
+        }
+        // toUpperCase将字符串中的所有字符转换为大写
+        hexString = hexString.toUpperCase();
+        int length = hexString.length() / 2;
+        // toCharArray将此字符串转换为一个新的字符数组。
+        char[] hexChars = hexString.toCharArray();
+        byte[] d = new byte[length];
+        for (int i = 0; i < length; i++) {
+            int pos = i * 2;
+            d[i] = (byte) (charToByte(hexChars[pos]) << 4 | charToByte(hexChars[pos + 1]));
+        }
+        return d;
+    }
+
+    /**
+     * 返回在指定字符的第一个发生的字符串中的索引，即返回匹配字符
+     * @param c
+     * @return
+     */
+    private static byte charToByte(char c) {
+        return (byte) "0123456789ABCDEF".indexOf(c);
+    }
     //最小公倍数:lest common multiple
     public static int lcm(int p,int q){
        /* calculation process
